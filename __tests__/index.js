2a2e38955cad4a47a6113d3d25db671b92c75083
baiest <juanballesteros2001@gmail.com>
2022-04-12 15:32:29 -0500

    Add other test

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,4 +3,5 @@ const sum = require('../index')
 describe('Test Sum works!', () => {
   test('3 + 4', () => expect(sum(3, 4)).toBe(7))
   test('5 - 6', () => expect(sum(5, -6)).toBe(-1))
+  test('2 + 2', () => expect(sum(2, 2)).toBe(4))
 })
